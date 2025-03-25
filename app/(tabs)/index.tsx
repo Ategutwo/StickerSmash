2447ac8b559eaa1f3f993bdb35a73ba7b245b72a
Ategutwo <alvintegutwodev@gmail.com>
2025-03-25 15:38:26 +0300

    (New Functionality)Adding Gestures Animation and Function to save to device

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,20 +3,30 @@ import ImageViewer from "@/components/ImageViewer";
 import {Image} from "expo-image"
 import { Text, View,StyleSheet } from "react-native";
 import * as ImagePicker from "expo-image-picker"
-import { useState } from "react";
+import { useState,useRef } from "react";
 import IconButton from "@/components/IconButton";
 import CirceButton from "@/components/CirceButton";
 import EmojiPicker from "@/components/EmojiPicker";
 import EmojiList from "@/components/EmojiList";
 import { type ImageSource } from "expo-image";
 import EmojiSticker from "@/components/EmojiSticker";
+import {GestureHandlerRootView} from "react-native-gesture-handler"
+import * as MediaLibrary from "expo-media-library"
+import {captureRef} from "react-native-view-shot"
 const PlaceHolderImage = require("@/assets/images/background-image.png")
 
 export default function Index() {
+  const imageRef = useRef<View>(null)
   const [selectedImage, setSelectedImage] = useState<String | undefined>(undefined)
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false)
   const [isModalVisible,setIsModalVisible] = useState<boolean>(false)
   const [pickedEmoji,setPickedEmoji] = useState<ImageSource| undefined>(undefined)
+  const [status, requestPermissions] = MediaLibrary.usePermissions();
+
+  if(!status){
+    requestPermissions()
+  }
+
   const pickImageAsync = async ()=>{
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes:["images"],
@@ -43,14 +53,27 @@ export default function Index() {
   }
   const onSaveImageAsync = async ()=>{
     //Save to device
+    try{
+      let localuri = await captureRef(imageRef,{height:400,quality:1})
+      await MediaLibrary.saveToLibraryAsync(localuri)
+      if(localuri){
+        alert("Screenshot saved!")
+      }
+    }
+    catch(e){
+      console.log(e)
+    }
   }
   return (
-    <View
+    <GestureHandlerRootView
       style={styles.container}
     >
       <View style={styles.imageContainer}>
-      <ImageViewer ImageSource={PlaceHolderImage} SelectedImage={selectedImage}/>
-      {pickedEmoji && <EmojiSticker stickerSource={pickedEmoji} imageSize={40}/>}
+        <View ref={imageRef} collapsable={false}>
+        <ImageViewer ImageSource={PlaceHolderImage} SelectedImage={selectedImage}/>
+        {pickedEmoji && <EmojiSticker stickerSource={pickedEmoji} imageSize={40}/>}
+        </View>
+      
       </View>
       {showAppOptions?<View  style={styles.optionsContainer}>
         <View style={styles.optionsRow}>
@@ -69,7 +92,7 @@ export default function Index() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onCloseModal={onModalClose} onSelect={setPickedEmoji}/>
       </EmojiPicker>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,14 +1,51 @@
+import { transform } from "@babel/core"
 import { Image,type ImageSource } from "expo-image"
 import {View} from "react-native"
+import {Gesture, GestureDetector} from "react-native-gesture-handler"
+import Animate,{useAnimatedStyle,useSharedValue,withSpring } from "react-native-reanimated"
 type Props = {
     imageSize: number,
     stickerSource: ImageSource,
 }
 
 export default function EmojiSticker({imageSize,stickerSource}:Props) {
+    let scaleSize= useSharedValue(imageSize);
+    let translateX = useSharedValue(0)
+    let translateY = useSharedValue(0)
+    const imageStyle = useAnimatedStyle(()=>{
+        return {
+            width: withSpring(scaleSize.value),
+            height: withSpring(scaleSize.value)
+        }
+    })
+    let doubleTap = Gesture.Tap().numberOfTaps(2)
+    .onStart(()=>{
+        if(scaleSize.value !== imageSize*2){
+            scaleSize.value = imageSize *2;
+        }
+        else{
+            scaleSize.value = Math.round(scaleSize.value/2)
+        }
+    })
+    const drag = Gesture.Pan().onChange(event=>{
+        translateX.value += event.changeX
+        translateY.value += event.changeY
+    })
+    const containerStyle = useAnimatedStyle(()=>{
+        return {
+            transform:[
+                {translateX: translateX.value},
+                {translateY:translateY.value}
+            ]
+        }
+    })
   return (
-    <View style={{top:-350}}>
-    <Image source={stickerSource} style={{width:imageSize,height:imageSize}}/>
-    </View>
+    <GestureDetector gesture={drag}>
+    <Animate.View  style={[containerStyle,{top:-350}]}>
+        <GestureDetector gesture={doubleTap}>
+    <Animate.Image resizeMode="contain" source={stickerSource} style={[imageStyle,{width:imageSize,height:imageSize}]}/>
+    </GestureDetector>
+    </Animate.View>
+    </GestureDetector>
   )
 }
